da1c91970bf32b0e9f27c479f2b73e36d8ad101e
diogocorigo <diogoacorigo@gmail.com>
2025-07-04 15:02:03 +0100

    no-message

--- src/components/partials/MySocials.vue
+++ src/components/partials/MySocials.vue
@@ -3,7 +3,7 @@ const urls = {
     linkedin: "https://www.linkedin.com/in/diogo-corigo/",
     github: "https://github.com/diogocorigo",
     email: "mailto:diogoacorigo@gmail.com",
-    resume: ""
+    resume: "/cv_diogocorigo.pdf"
 }
 </script>
 
@@ -50,12 +50,13 @@ const urls = {
                 </svg>Email
             </a>
 
-            <div
-                class="w-full p-4 py-3 rounded-br-2xl bg-emerald-950 text-emerald-50 flex items-center gap-2 font-display" />
-            <!-- <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512">
+            <a :href="urls.resume"
+                class="w-full p-4 py-3 rounded-bl-2xl bg-emerald-950 text-emerald-50 flex items-center gap-2 font-display cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512">
                     <path fill="#ecfdf5 "
                         d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM216 232l0 102.1 31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31L168 232c0-13.3 10.7-24 24-24s24 10.7 24 24z" />
-                </svg>Resume -->
+                </svg>Resume
+            </a>
         </div>
     </section>
 </template>
